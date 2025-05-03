20968f573423dd18add348470c155ab573381d42
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-05-02 20:23:24 -0700

    Add project files.

---
+++ COMP003B.Assignment6/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace COMP003B.Assignment6.Models
+{
+	public class Movie
+	{
+		public int MovieId { get; set; }
+
+		public string MovieName { get; set; }
+
+		public string MovieDescription { get; set; }
+
+		public virtual ICollection<Movie>? MovieDirector { get; set; }
+	}
+}
