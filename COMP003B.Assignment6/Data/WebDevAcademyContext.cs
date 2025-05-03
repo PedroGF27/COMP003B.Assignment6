0b507d011edea2c200c61f9d97b1f08a7446097f
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-05-02 23:12:25 -0700

    All I could do, I was having problems with the controllers and installing migrations and I could not figure out the solution

---
+++ COMP003B.Assignment6/Data/WebDevAcademyContext.cs
@@ -0,0 +1,17 @@
+﻿using COMP003B.Assignment6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.Assignment6.Data
+{
+	public class WebDevAcademyContext : DbContext
+	{
+		public WebDevAcademyContext(DbContextOptions<WebDevAcademyContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Movie> Movies { get; set; }
+		public DbSet<MovieDirector> Directors { get; set; }
+		public DbSet<MovieReview> Reviews { get; set; }
+
+	}
+}
