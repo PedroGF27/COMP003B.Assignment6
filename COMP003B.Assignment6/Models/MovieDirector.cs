414a9c8527c04cd53889859b8c9e2aab16886863
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-05-02 20:42:02 -0700

    Created the models for my theme and added secret.json

---
+++ COMP003B.Assignment6/Models/MovieDirector.cs
@@ -0,0 +1,13 @@
+﻿namespace COMP003B.Assignment6.Models
+{
+	public class MovieDirector
+	{
+		public int MovieDirectorId { get; set; }
+
+		public string MovieDirectorName { get; set; }
+
+		public string MovieDirectorDescription { get; set; }
+
+		public virtual ICollection<MovieDirector>? Movie { get; set; }
+	}
+}
---
+++ COMP003B.Assignment6/Models/MovieReview.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment6.Models
+{
+	public class MovieReview
+	{
+		public int UserId { get; set; }
+		public int ReviewId { get; set; }
+		[Range(1, 10, ErrorMessage = "Rating must be between 1-10.")]
+		public int Rating { get; set; }
+
+		public virtual Movie? Movie { get; set; }
+		public virtual MovieDirector? MovieDirector { get; set; }
+
+	}
+}
